f07e7480e5c72cd55f865f0f161db6c63b54bd8f
Daria Berseneva <berseneva138@yandex.ru>
2022-04-18 23:12:04 +0700

    89/1

--- 18.04/86/index.php
+++ 18.04/86/index.php
@@ -1,11 +1,14 @@
 <?php
-	namespace Core\Storage;
-	use DataBase;
-	class Model
+	namespace Project;
+	
+	use \Resource\Controller\Page as ControllerPage;
+	use \Resource\Model\Page as MoselPage;
+	class Test
 	{
 		public function __construct()
 		{
-			$database  = new DataBase;
+			$pageController  = new ControllerPage;
+			$pageModel       = new ModelPage;
 		}
 	}
 ?>
